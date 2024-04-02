5a91d2e2bee29a2e3d9a408bab0aa80b4d78aaf0
a-good-omen <146872119+a-good-omen@users.noreply.github.com>
2024-04-02 23:24:52 +0530

    Update helper.py

--- helper.py
+++ helper.py
@@ -4,14 +4,11 @@ import random
 def wordpicker():
     wordfile=open("wordfile.txt")
     wordlist=[wrd.rstrip() for wrd in wordfile]
-
     word=random.choice(wordlist)
-
     return word
 
 
 def parts():
     rawparts=open('hangmanparts.txt').readlines()
     parts_list=[rawparts[u_lim-7:u_lim] for u_lim in range(7,49,7)]
-
     return parts_list
