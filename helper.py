d0f0f7f812488a5e4e131fa05ef60cb6c92cdec0
a-good-omen <146872119+a-good-omen@users.noreply.github.com>
2024-04-05 20:12:14 +0530

    Update helper.py

--- helper.py
+++ helper.py
@@ -12,3 +12,12 @@ def parts():
     rawparts=open('hangmanparts.txt').readlines()
     parts_list=[rawparts[u_lim-7:u_lim] for u_lim in range(7,49,7)]
     return parts_list
+
+        
+def display_changer(curr_display,status,guess):
+	for index,letter in status:
+		if letter==guess:
+			curr_display[index]=letter
+			status.remove((index,letter))
+			break
+	return ''.join(curr_display),status
